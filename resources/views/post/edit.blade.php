86f3007047deaf9f5127271806300efa32e389df
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:05:38 +0900

    更新画面作成

---
+++ resources/views/post/edit.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUDアプリ</title>
+</head>
+
+<body>
+    <h1>更新ページ</h1>
+    <hr>
+    {{-- <form action="{{ route('post.update') }}" method="POST"> --}}
+        @csrf
+        @method('PUT')
+        <label for="title">タイトル:</label>
+        <input type="text" name="title" id="title" value="{{ old('title', $post->title) }}">
+        <br>
+        <label for="body">本文</label>
+        <textarea name="body" id="body" cols="30" rows="10">{{ old('body', $post->body) }}</textarea>
+        <br>
+        <button>更新</button>
+    </form>
+</body>
+
+</html>
