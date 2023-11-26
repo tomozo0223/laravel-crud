3025bec1511eaa84d3aa40b4dc63e293d8c46279
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:17:59 +0900

    deleteルート追加

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/post/create', [PostController::class, 'create'])->name('post.create
 Route::post('/post', [PostController::class, 'store'])->name('post.store');
 Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
 Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-Route::PUT('/post/{post}', [PostController::class, 'update'])->name('post.update');
+Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
+Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
