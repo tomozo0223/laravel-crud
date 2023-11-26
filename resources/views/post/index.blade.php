3777941715a1c4f47ef15b2a335e88036f128783
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:01:13 +0900

    一覧ページ作成

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUDアプリ</title>
+</head>
+
+<body>
+    <h1>一覧ページ</h1>
+    @foreach ($posts as $post)
+        <h2>{{ $post->title }}</h2>
+        <p>{{ $post->body }}</p>
+        <hr>
+    @endforeach
+</body>
+
+</html>
