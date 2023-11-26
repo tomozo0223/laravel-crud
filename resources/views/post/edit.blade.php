7940442f7f4cae6b7fbbc31e65503f5d3c3dd0ec
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:45:00 +0900

    更新時のバリデーションエラーメッセージ表示

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -15,9 +15,15 @@
         @csrf
         @method('PUT')
         <label for="title">タイトル:</label>
+        @error('title')
+            <p style="color:red">タイトルまたは、5文字以内で記入してください</p>
+        @enderror
         <input type="text" name="title" id="title" value="{{ old('title', $post->title) }}">
         <br>
         <label for="body">本文</label>
+        @error('body')
+            <p style="color:red">本文または、20文字以内で記入してください</p>
+        @enderror
         <textarea name="body" id="body" cols="30" rows="10">{{ old('body', $post->body) }}</textarea>
         <br>
         <button>更新</button>
