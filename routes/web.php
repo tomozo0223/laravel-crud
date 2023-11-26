77da7562ac5ca5c0629fac72e1246074f85bd602
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:10:21 +0900

    updateルート追加

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/post', [PostController::class, 'index'])->name('post.index');
 Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
 Route::post('/post', [PostController::class, 'store'])->name('post.store');
 Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+Route::PUT('/post/{post}', [PostController::class, 'update'])->name('post.update');
