de876679950a28239cc552d02757bf5a8ce219c7
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:41:15 +0900

    バリデーションエラーメッセージ表示

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -14,9 +14,15 @@
     <form action="{{ route('post.store') }}" method="POST">
         @csrf
         <label for="title">タイトル:</label>
+        @error('title')
+            <p style="color:red">タイトルまたは、5文字以内で記入してください</p>
+        @enderror
         <input type="text" name="title" id="title">
         <br>
         <label for="body">本文</label>
+        @error('body')
+            <p style="color:red">本文または、20文字以内で記入してください</p>
+        @enderror
         <textarea name="body" id="body" cols="30" rows="10"></textarea>
         <br>
         <button>送信</button>
