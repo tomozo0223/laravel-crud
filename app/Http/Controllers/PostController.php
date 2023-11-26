502d18ccef27c1829c427cb5cf6396cdaa7ac527
tomoki <tmdesign.0223@gmail.com>
2023-11-27 00:00:53 +0900

    PostController作成,index,create,storeメソッド作成

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('post.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('post.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        return to_route('post.index');
+    }
+}
